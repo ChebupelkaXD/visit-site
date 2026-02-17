cbc6e17305fda3acd05b65b14a6d15e741ecd429
ChebupelkaXD <101295814+ChebupelkaXD@users.noreply.github.com>
2026-02-18 01:48:13 +0300

    upd: рефакторинг тестов

--- tests/handlers_test.go
+++ tests/handlers_test.go
@@ -11,66 +11,63 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type Tests []struct {
+	desc    string
+	methods []string
+	target  string
+	body    io.Reader
+	expect  int
+}
+
 func TestUnitHandlersPage(t *testing.T) {
-	t.Run("Тест рендера страниц TemplateRender", func(t *testing.T) {
+
+	t.Run("Тест TemplateRender", func(t *testing.T) {
 		w := httptest.NewRecorder()
 
-		tests := []struct {
-			tmplName   string
-			wantStatus int
-		}{
-			{
-				"index.html",
-				200},
-			{
-				"notfound.html",
-				200},
-			{
-				"about.html",
-				200},
-			{
-				"skills.html",
-				200},
-			{
-				"projects.html",
-				200},
-			{
-				"experience.html",
-				200},
-			{
-				"contacts.html",
-				200},
-		}
-
-		for _, test := range tests {
-			handlers.TemplateRender(w, test.tmplName)
-
-			if w.Code != test.wantStatus {
-				t.Errorf("Expected status %q, got %q", test.wantStatus, w.Code)
-			}
-		}
+		handlers.TemplateRender(w, "invisible.html")
+		assert.Equal(t, w.Code, http.StatusInternalServerError)
 	})
 
-	t.Run("Тест отображения страницы NotFound", func(t *testing.T) {
+	t.Run("Тест NotFound", func(t *testing.T) {
 		req := httptest.NewRequest(http.MethodGet, "/not-found-page", nil)
 		w := httptest.NewRecorder()
 
 		handlers.NotFound(w, req)
 
-		if w.Code != http.StatusNotFound {
-			t.Errorf("Expcted status %v, got %v", http.StatusNotFound, w.Code)
+		assert.Equal(t, w.Code, http.StatusNotFound)
+	})
+
+	t.Run("Тест видимых страниц", func(t *testing.T) {
+		tests := []struct {
+			desc     string
+			funcName func(w http.ResponseWriter, r *http.Request)
+			target   string
+			body     io.Reader
+			expect   int
+		}{
+			{"Home", handlers.Home, "/", nil, http.StatusOK},
+			{"About", handlers.About, "/about", nil, http.StatusOK},
+			{"Skills", handlers.Skills, "/skills", nil, http.StatusOK},
+			{"Experience", handlers.Experience, "/experience", nil, http.StatusOK},
+			{"Contacts", handlers.Contacts, "/contacts", nil, http.StatusOK},
 		}
+
+		for _, tt := range tests {
+			t.Run(tt.desc, func(t *testing.T) {
+				req := httptest.NewRequest(http.MethodGet, tt.target, tt.body)
+				w := httptest.NewRecorder()
+				tt.funcName(w, req)
+
+				assert.Equal(t, w.Code, tt.expect)
+				assert.Contains(t, w.Header().Get("Content-Type"), "text/html")
+			})
+		}
+
 	})
 }
 
 func TestUnitHandlersContactAPI(t *testing.T) {
-	tests := []struct {
-		desc    string
-		methods []string
-		target  string
-		body    io.Reader
-		expect  int
-	}{
+	tests := Tests{
 		{"Не разрешенный запрос", []string{http.MethodGet, http.MethodDelete, http.MethodPatch, http.MethodPut, http.MethodHead, http.MethodOptions}, "/api/contact", nil, http.StatusInternalServerError},
 		{"Битый JSON", []string{http.MethodPost}, "/api/contact", strings.NewReader(`{"name": "Paul", "email": "example@mail.ru", "message": "Hello"`), http.StatusBadRequest},
 		{"Не полный JSON", []string{http.MethodPost}, "/api/contact", strings.NewReader(`{"name": "", "email": "", "message": ""`), http.StatusBadRequest},
